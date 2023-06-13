7b2de18897d7112d86eb1f19f18a8bf98fdba4e1
George <george.neokq@gmail.com>
2023-06-13 11:38:54 +0800

    feat: read signatures from sigs folder

--- unginxed/__main__.py
+++ unginxed/__main__.py
@@ -1,11 +1,53 @@
-from .sigs.alias_lfi import matcher
+from importlib import import_module
+from os import listdir, path
+from typing import Callable
 from .nginx_config import NginxConfig
+from .directive import Directive
 
 
 def main():
-    config = NginxConfig("examples/default.conf")
-    signature_result = matcher(config.directives)
-    print(signature_result)
+    config = NginxConfig(path.join(__file__, '..', '..', 'examples', 'default.conf'))
+    directives = config.directives
+    signatures = get_signatures()
+
+    results = [signature(directives) for signature in signatures]
+    print(results)
+
+
+def get_signatures(signatures_folder=None) -> list[Callable[[list[Directive]], None]]:
+    """
+    Retrieves a list of signatures.
+    Each signature should be a python file in the specified signatures folder,
+    containing a function named "matcher". Each matcher function takes in
+    a list of Directive objects as a parameter.
+
+    Args:
+        signatures_folder: If not provided, defaults to 'sigs' folder.
+
+    Returns:
+        list[Callable[[list[Directive]], None]]: _description_
+    """
+    signatures = []
+
+    if signatures_folder is None:
+        signatures_folder = path.join(__file__, '..', 'sigs')
+
+    filenames = list(filter(
+        lambda filename: filename.endswith('.py') and not filename.endswith('__'),
+        listdir(signatures_folder))
+    )
+
+    for filename in filenames:
+        try:
+            signature = import_module(
+                f'.sigs.{filename.rstrip(".py")}',
+                package=__package__
+            )
+            signatures.append(signature.matcher)
+        except Exception:
+            pass
+
+    return signatures
 
 
 if __name__ == "__main__":
--- unginxed/nginx_config.py
+++ unginxed/nginx_config.py
@@ -1,5 +1,6 @@
 import crossplane
 from .directive import Directive, DirectiveDict, DirectiveUtil
+from os import path
 
 
 class NginxConfig:
@@ -10,8 +11,13 @@ class NginxConfig:
         Args:
             filepath (str): Absolute or relative path to the config file
         """
+
+        if not path.exists(filepath):
+            raise IOError(f'Invalid file path "{filepath}" provided.')
+
         self.filepath: str = filepath
         self.raw: dict | None = crossplane.parse(filepath)
+
         self.directives: list[Directive] = []
 
         config: list[DirectiveDict] = self.raw["config"][0]["parsed"]
