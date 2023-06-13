02b3997e2f676f622b7076ee8eefcf95e1a35ed5
George <george.neokq@gmail.com>
2023-06-13 10:44:04 +0800

    refactor: remove unused methods from NginxConfig

--- unginxed/nginx_config.py
+++ unginxed/nginx_config.py
@@ -26,27 +26,6 @@ class NginxConfig:
                 directive_dict
             )
 
-    def get_raw_config(self) -> dict | None:
-        return None if self.raw is None else self.raw['config'][0]
-
-    def get_directive(self, directive: str) -> dict | None:
-        '''
-        Retrives a dictonary relating to a directive
-        '''
-        if self.raw is None:
-            return None
-        for block in self.directives:
-            if 'directive' in block.keys() and block['directive'] == directive:
-                return block
-        return None
-
-    def get_directive_blocks(self, directive: str) -> list[dict]:
-        '''
-        Retrieves a list of blocks associated with a directive.
-        '''
-        directive_dict = self.get_directive(directive)
-        return [] if directive_dict is None else directive_dict['block']
-
     def __repr__(self) -> str:
         return str(self.raw)
 
