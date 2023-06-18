64e0375514270e0eff9b3e61f4d87a569f3accde
alloygoh <106863675+alloygoh@users.noreply.github.com>
2023-06-18 21:26:28 +0800

    sigs: add raw backend response sig

---
+++ unginxed/sigs/raw_backend_response_reading.py
@@ -0,0 +1,18 @@
+from ..directive import DirectiveUtil
+from ..nginx_config import NginxConfig
+from ..signature import Signature, SignatureBuilder
+
+
+def matcher(config: NginxConfig) -> Signature:
+    signature_builder = SignatureBuilder(config.raw).set_name('Raw Backend Response Reading') \
+                                          .set_reference_url('https://blog.detectify.com/2020/11/10/common-nginx-misconfigurations') \
+                                          .set_description('If Nginx does not understand the request type, usage of proxy_hide_header and proxy_intercept_errors will fail to hide potential sensitive information')
+
+    hide_headers_directive = DirectiveUtil.get_directives('proxy_hide_header', config.directives)
+    for directive in hide_headers_directive:
+        sub_directives = [sub_directive.directive for sub_directive in directive.parent.block]
+        print(sub_directives)
+        if 'proxy_intercept_errors' in sub_directives:
+            signature_builder.add_flagged(directive, config.raw)
+
+    return signature_builder.build()
