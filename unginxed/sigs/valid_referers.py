4e16eac7c8f4ac1c31e6be19a4e4e6de538f15cd
alloygoh <106863675+alloygoh@users.noreply.github.com>
2023-06-18 15:27:56 +0800

    sigs: add referer signature

---
+++ unginxed/sigs/valid_referers.py
@@ -0,0 +1,17 @@
+from ..directive import DirectiveUtil
+from ..nginx_config import NginxConfig
+from ..signature import Signature, SignatureBuilder
+
+
+def matcher(config: NginxConfig) -> Signature:
+    signature_builder = SignatureBuilder(config.raw).set_name('valid referers') \
+                                          .set_reference_url('https://github.com/yandex/gixy/blob/master/docs/en/plugins/validreferers.md') \
+                                          .set_description('none is an allowed referer amongst other filtered referers')
+
+    referers_directives = DirectiveUtil.get_directives('valid_referers', config.directives)
+    print(referers_directives)
+    for directive in referers_directives:
+        if len(directive.args) > 1 and 'none' in directive.args:
+            signature_builder.add_flagged(directive, config.raw)
+
+    return signature_builder.build()
