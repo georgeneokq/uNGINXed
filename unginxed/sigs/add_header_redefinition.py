47f1aeb1af3c765266b59bbbce808811fa42ddcf
alloygoh <106863675+alloygoh@users.noreply.github.com>
2023-06-18 21:14:50 +0800

    sigs: add add_header redefinition sig

---
+++ unginxed/sigs/add_header_redefinition.py
@@ -0,0 +1,18 @@
+from ..directive import DirectiveUtil, Directive
+from ..nginx_config import NginxConfig
+from ..signature import Signature, SignatureBuilder
+
+signature_builder: SignatureBuilder = None
+
+
+def matcher(config: NginxConfig) -> Signature:
+    signature_builder = SignatureBuilder(config.raw).set_name('add_header Redefinition') \
+                                          .set_reference_url('https://github.com/yandex/gixy/blob/master/docs/en/plugins/addheaderredefinition.md') \
+                                          .set_description('Lower level add_header redefinition overwrites higher level add_header definitions, causing high level definitions to be lost.')
+    add_header_directives = DirectiveUtil.get_directives('add_header', config.directives)
+    for directive in add_header_directives:
+        temp = [d for d in directive.parent.parent.block if d.directive == 'add_header']
+        if temp:
+            signature_builder.add_flagged(directive, config.raw)
+
+    return signature_builder.build()
