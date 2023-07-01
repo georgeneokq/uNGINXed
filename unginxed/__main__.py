d5bc6d4f85c83ed43e68ccf445e432121378faa0
Kenneth Lee <60289339+kenleejl@users.noreply.github.com>
2023-07-01 09:21:16 +0800

    feat: add terminal interaction
    * feat: signature severity
    
    * WIP: output in CLI
    
    * refactor: enum severity
    
    * feat: Tables for Signature
    
    * add: url footer and description
    
    * Add: rich console print
    
    * chore: use fstrings and remove unused imports
    
    ---------
    
    Co-authored-by: alloygoh <106863675+alloygoh@users.noreply.github.com>

--- unginxed/__main__.py
+++ unginxed/__main__.py
@@ -1,26 +1,93 @@
 import argparse as ap
+from pathlib import Path
+from rich import print as rprint
 
 from .nginx_config import NginxConfig
-from .report import generate_pdf_report
+from .report import generate_pdf_report, report_summary_cli, report_verbose_cli
 from .signature import get_signatures
 
 
+UNGINXED_VERSION = "0.1.1"
+UNGINXED_LOGO = """
+        _   _  _____ _____ _   ___   __        _ 
+       | \ | |/ ____|_   _| \ | \ \ / /       | |
+  _   _|  \| | |  __  | | |  \| |\ V / ___  __| |
+ | | | | . ` | | |_ | | | | . ` | > < / _ \/ _` |
+ | |_| | |\  | |__| |_| |_| |\  |/ . \  __/ (_| |
+  \__,_|_| \_|\_____|_____|_| \_/_/ \_\___|\__,_| 
+
+          """
+
+
 def main():
-    argument_parser = ap.ArgumentParser()
-    argument_parser.add_argument('file', type=str, help="Path to NGINX configuration file")
-    argument_parser.add_argument('--pdf-output', default="reports", type=str, help="Optional PDF report output directory")
+    argument_parser = ap.ArgumentParser(
+        prog=UNGINXED_LOGO,
+        description="A tool to detect misconfigurations in NGINX configuration files",
+        epilog="Example: poetry run python unginxed /etc/nginx/nginx.conf",
+    )
+    argument_parser.add_argument(
+        "file", type=str, help="Path to NGINX configuration file"
+    )
+    argument_parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"READY TO BE uNGINXed? This is version {UNGINXED_VERSION}",
+    )
+    argument_parser.add_argument(
+        "-o",
+        "--pdf-output",
+        default="reports",
+        type=str,
+        help="Optional PDF report output directory",
+    )
+    argument_parser.add_argument(
+        "-vv",
+        "--verbose",
+        default="verbose",
+        type=str,
+        help="Prints nginx configuration report",
+    )
+    argument_parser.add_argument(
+        "-s",
+        "--summary",
+        default="summary",
+        type=str,
+        help="Prints summary report",
+    )
     args = argument_parser.parse_args()
     filepath = args.file
     pdf_output_path = args.pdf_output
 
     config = NginxConfig(filepath)
+    print(UNGINXED_LOGO)
     signatures = get_signatures()
 
     results = [signature(config) for signature in signatures]
+    if pdf_output_path is not None:
+        report_path = generate_pdf_report(
+            config, results, output_folder=pdf_output_path
+        )
 
-    report_path = generate_pdf_report(config, results, output_folder=pdf_output_path)
+    total_flagged = sum(len(result.flagged) for result in results)
 
-    print(f'Generated report at {report_path}')
+    if total_flagged == 0:
+        rprint("No misconfigurations found. You are safe FOR NOW!\n\n")
+    else:
+        rprint(
+            f"You have been NGINXED! {total_flagged} directive flagged in your configuration\n\n")
+
+    if args.summary:
+        report_summary_cli(results)
+
+    if args.verbose:
+        report_verbose_cli(config, results)
+
+    if report_path:
+        report_path = Path(report_path)
+        rprint(
+            f"Jinxed with bad luck? :thumbs_down:\nCheck your NGINX report at: [link=file://{str(report_path)}] {str(report_path)} [/link]")
+        # Print the clickable URL
 
 
 if __name__ == "__main__":
